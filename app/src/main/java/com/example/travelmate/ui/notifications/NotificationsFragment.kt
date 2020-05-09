14bfae8791fb4084d6da112955e486524d1e6e80
StefiNicoara <StefiNicoara@users.noreply.github.com>
2020-05-09 15:33:38 +0300

    dete viremodel providers

--- app/src/main/java/com/example/travelmate/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/travelmate/ui/notifications/NotificationsFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.travelmate.R
 
 class NotificationsFragment : Fragment() {
@@ -19,13 +16,7 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
 }
